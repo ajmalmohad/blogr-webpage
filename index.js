573066d1af07a75bd007356a2411b973a3d48fa6
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-06-06 19:51:25 +0530

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+let navbar = document.getElementsByClassName('navlinks')[0];
+let burger = document.getElementsByClassName('burger')[0];
+let active =  false;
+burger.addEventListener('click',()=>{
+    active = !active;
+    if(active){
+        burger.innerHTML = "<img src='./images/icon-close.svg'/>";
+    }
+    else{
+        burger.innerHTML = "<img src='./images/icon-hamburger.svg'/>";
+    }
+    navbar.classList.toggle("active");
+})
